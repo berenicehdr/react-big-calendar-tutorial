c790ae4cd115ccef16b5d8a3354354da3f37367f
Berenice H <berenice.hdr@MacBook-Pro-de-Berenice.local>
2024-06-26 17:14:11 -0600

    modifyng data

--- src/Components/BasicCalendar/BasicCalendar.tsx
+++ src/Components/BasicCalendar/BasicCalendar.tsx
@@ -3,13 +3,13 @@ import Calendar from "../Calendar";
 
 const events = [
   {
-    start: moment("2023-03-18T10:00:00").toDate(),
-    end: moment("2023-03-18T11:00:00").toDate(),
+    start: moment("2024-06-26T10:00:00").toDate(),
+    end: moment("2024-06-26T11:00:00").toDate(),
     title: "MRI Registration",
   },
   {
-    start: moment("2023-03-18T14:00:00").toDate(),
-    end: moment("2023-03-18T15:30:00").toDate(),
+    start: moment("2024-06-26T14:00:00").toDate(),
+    end: moment("2024-06-26T15:30:00").toDate(),
     title: "ENT Appointment",
   },
 ];
